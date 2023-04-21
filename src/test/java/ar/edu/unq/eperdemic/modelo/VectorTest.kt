4651481c6d7483955ddd76dd6c70a273b19cc5d8
Pablo <pablo.bragoni@gmail.com>
2023-04-20 22:43:46 -0300

    agregue estaSano test en vector

--- src/test/java/ar/edu/unq/eperdemic/modelo/VectorTest.kt
+++ src/test/java/ar/edu/unq/eperdemic/modelo/VectorTest.kt
@@ -1,19 +1,38 @@
 package ar.edu.unq.eperdemic.modelo
 
+import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernateEspecieDAO
+import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernateUbicacionDAO
+import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernateVectorDAO
+import ar.edu.unq.eperdemic.services.impl.EspecieServiceImpl
+import ar.edu.unq.eperdemic.services.impl.UbicacionServiceImpl
+import ar.edu.unq.eperdemic.services.impl.VectorServiceImpl
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-
 import org.junit.jupiter.api.Assertions.*
+
+import ar.edu.unq.eperdemic.utils.DataServiceHibernate
+import org.junit.jupiter.api.Assertions
+
 
 class VectorTest {
 
+    private val hibernateVectorDAO = HibernateVectorDAO()
+    private val hibernateUbicacionDAO = HibernateUbicacionDAO()
+    private val hibernateEspecieDAO = HibernateEspecieDAO()
+    private val ubicacionServiceImpl = UbicacionServiceImpl(hibernateUbicacionDAO)
+    private val vectorServiceImpl = VectorServiceImpl(hibernateVectorDAO)
+    private val especieServiceImpl = EspecieServiceImpl(hibernateEspecieDAO)
+    private val dataServiceHibernate = DataServiceHibernate()
+
     @BeforeEach
     fun setUp() {
+        dataServiceHibernate.crearSetDeDatosIniciales()
     }
 
     @AfterEach
     fun tearDown() {
+        dataServiceHibernate.eliminarTodo()
     }
 
     @Test
@@ -26,6 +45,7 @@ class VectorTest {
 
     @Test
     fun esContagioExitoso() {
+
     }
 
     @Test
@@ -34,6 +54,11 @@ class VectorTest {
 
     @Test
     fun estaSano() {
+        val vector = vectorServiceImpl.recuperarVector(7)
+        val especie = especieServiceImpl.recuperarEspecie(1)
+        assertTrue(vector.estaSano())
+        vector.infectarCon(especie)
+        assertFalse(vector.estaSano())
     }
 
     @Test
