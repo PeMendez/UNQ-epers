23cc03561e46a5fffff3d32629def52de029a7a2
Pablo <pablo.bragoni@gmail.com>
2023-04-20 21:59:49 -0300

    agrego test de vector modelo

---
+++ src/test/java/ar/edu/unq/eperdemic/modelo/VectorTest.kt
@@ -0,0 +1,46 @@
+package ar.edu.unq.eperdemic.modelo
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class VectorTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun intentarInfectar() {
+    }
+
+    @Test
+    fun infectarCon() {
+    }
+
+    @Test
+    fun esContagioExitoso() {
+    }
+
+    @Test
+    fun porcentajeDeContagioExitoso() {
+    }
+
+    @Test
+    fun estaSano() {
+    }
+
+    @Test
+    fun tieneEfermedad() {
+    }
+
+    @Test
+    fun mover() {
+    }
+}
