a64cc875bcb341c8faf5a87615263e48ed0c2f25
Franco Marengo <francomarengo.21@gmail.com>
2023-04-29 02:13:14 -0300

    Agrego tests de EspecieServiceImpl y elimino IDs harcodeadas, todos los tests corriendo.

--- src/main/java/ar/edu/unq/eperdemic/services/impl/EspecieServiceImpl.kt
+++ src/main/java/ar/edu/unq/eperdemic/services/impl/EspecieServiceImpl.kt
@@ -1,17 +1,21 @@
 package ar.edu.unq.eperdemic.services.impl
 
 import ar.edu.unq.eperdemic.modelo.Especie
+import ar.edu.unq.eperdemic.modelo.exceptions.NoExisteElid
+import ar.edu.unq.eperdemic.modelo.exceptions.NoExisteUnaEspecieLider
 import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernateEspecieDAO
 import ar.edu.unq.eperdemic.services.EspecieService
 import ar.edu.unq.eperdemic.services.runner.TransactionRunner.runTrx
+import java.util.NoSuchElementException
 
 class EspecieServiceImpl(val hibernateEspecieDAO: HibernateEspecieDAO) : EspecieService {
 
     override fun recuperarEspecie(id: Long): Especie {
-        return runTrx { hibernateEspecieDAO.recuperar(id)}
+        return runTrx { hibernateEspecieDAO.recuperar(id)?: throw NoExisteElid("el id buscado no existe en la base de datos") }
     }
 
-    override fun cantidadDeInfectados(especieId: Long ): Int{
+    override fun cantidadDeInfectados(especieId: Long ): Int {
+        runTrx { hibernateEspecieDAO.recuperar(especieId)?: throw NoExisteElid("el id buscado no existe en la base de datos") }
         return runTrx { hibernateEspecieDAO.cantidadDeInfectados(especieId) }
     }
 
@@ -21,7 +25,13 @@ class EspecieServiceImpl(val hibernateEspecieDAO: HibernateEspecieDAO) : Especie
 
 
     override fun especieLider(): Especie {
-        return runTrx { hibernateEspecieDAO.especieLider() }
+        return runTrx {
+            try {
+                hibernateEspecieDAO.especieLider()
+            } catch (e: NoSuchElementException) {
+                throw NoExisteUnaEspecieLider("No hay una especie lider actualmente.")
+            }
+        }
     }
 
 
