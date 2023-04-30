81c10af0977890fc98b5dede414cabf0d4799bd8
Ornella Mendez <hope.peque@gmail.com>
2023-04-30 15:29:25 -0300

    patogenoTest sin idHardcodeados

--- src/test/java/ar/edu/unq/eperdemic/services/impl/PatogenoServiceTest.kt
+++ src/test/java/ar/edu/unq/eperdemic/services/impl/PatogenoServiceTest.kt
@@ -1,7 +1,6 @@
 package ar.edu.unq.eperdemic.services.impl
 
-import ar.edu.unq.eperdemic.modelo.Patogeno
-import ar.edu.unq.eperdemic.modelo.TipoDeVector
+import ar.edu.unq.eperdemic.modelo.*
 import ar.edu.unq.eperdemic.modelo.exceptions.NingunVectorAInfectarEnLaUbicacionDada
 import ar.edu.unq.eperdemic.modelo.exceptions.NoExisteElid
 import ar.edu.unq.eperdemic.modelo.exceptions.NoPuedeEstarVacioOContenerCaracteresEspeciales
@@ -20,30 +19,44 @@ class PatogenoServiceTest {
     private val especieServiceImpl = EspecieServiceImpl(hibernateEspecieDAO)
     private val ubicacionDAO = HibernateUbicacionDAO()
     private val ubicacionService = UbicacionServiceImpl(ubicacionDAO)
+    private var patogenoVirusCruciartus = Patogeno("VirusCruciartus")
+    private lateinit var patogenoMalDeDragon: Patogeno
+    private lateinit var ubicacionPrivateDrive: Ubicacion
+    private lateinit var ubicacionLaMadriguera: Ubicacion
+    private lateinit var vectorHarryPotter: Vector
+    private lateinit var vectorRonWeasley: Vector
+
 
     @BeforeEach
     fun setUp() {
+        Random.switchModo(false)
         dataService.crearSetDeDatosIniciales()
+        patogenoMalDeDragon = patogenoService.crearPatogeno(Patogeno("MalDeDragon"))
+        ubicacionPrivateDrive = ubicacionService.crearUbicacion("PrivateDrive")
+        vectorHarryPotter = vectorServiceImpl.crearVector(TipoDeVector.Persona, ubicacionPrivateDrive.id!!)
+        ubicacionLaMadriguera = ubicacionService.crearUbicacion("ubicacionLaMadriguera")
+
     }
 
     @Test
     fun cuandoSeCreaUnPatogenoSeLeAsignaUnId() {
-        var patogenoVirus = Patogeno("Virus")
-        patogenoVirus = patogenoService.crearPatogeno(patogenoVirus)
+        val patogenoVirus = patogenoService.crearPatogeno(patogenoVirusCruciartus)
 
         Assertions.assertNotNull(patogenoVirus.id)
     }
 
     @Test
     fun recuperarPatogenoTest(){
-        val patogenoRecuperado = patogenoService.recuperarPatogeno(1)
 
-        Assertions.assertEquals(patogenoRecuperado.id, 1)
-        Assertions.assertEquals(patogenoRecuperado.tipo, "tipo1")
-        Assertions.assertEquals(patogenoRecuperado.cantidadDeEspecies, 2)
-        Assertions.assertEquals(patogenoRecuperado.capacidadDeContagio, 100)
-        Assertions.assertEquals(patogenoRecuperado.capacidadDeBiomecanizacion,100)
-        Assertions.assertEquals(patogenoRecuperado.capacidadDeDefensa, 100)
+        val patogenoVirus = patogenoService.crearPatogeno(patogenoVirusCruciartus)
+        val patogenoRecuperado = patogenoService.recuperarPatogeno(patogenoVirus.id!!)
+
+        Assertions.assertEquals(patogenoVirus.id, patogenoRecuperado.id)
+        Assertions.assertEquals(patogenoVirus.tipo, patogenoRecuperado.tipo)
+        Assertions.assertEquals(patogenoVirus.cantidadDeEspecies, patogenoRecuperado.cantidadDeEspecies)
+        Assertions.assertEquals(patogenoVirus.capacidadDeContagio, patogenoRecuperado.capacidadDeContagio)
+        Assertions.assertEquals(patogenoVirus.capacidadDeBiomecanizacion,patogenoRecuperado.capacidadDeBiomecanizacion)
+        Assertions.assertEquals(patogenoVirus.capacidadDeDefensa, patogenoRecuperado.capacidadDeDefensa)
     }
 
     @Test
@@ -56,18 +69,18 @@ class PatogenoServiceTest {
 
     @Test
     fun seLeAgregaUnaEspecieAUnPatogenoTest(){
-        val especieGenerada = patogenoService.agregarEspecie(1,"EspecieViolenta", 2)
-        val listaEspecies = patogenoService.especiesDePatogeno(1)
+        val especieGenerada = patogenoService.agregarEspecie(patogenoMalDeDragon.id!!,"EspecieImperius", ubicacionPrivateDrive.id!!)
+        val listaEspecies = patogenoService.especiesDePatogeno(patogenoMalDeDragon.id!!)
 
-        Assertions.assertEquals(listaEspecies.size, 3 )
+        Assertions.assertEquals(1, listaEspecies.size)
         Assertions.assertEquals(listaEspecies.find { e -> e.id == especieGenerada.id }!!.id, especieGenerada.id)
     }
 
     @Test
     fun alAgregarleUnaEspecieAUnPatogenoSeIncrementaSuCantidadDeEspecies() {
-        val patogeno = patogenoService.recuperarPatogeno(1)
-        patogenoService.agregarEspecie(1, "virus", 2)
-        val patogenoRecuperado = patogenoService.recuperarPatogeno(1)
+        val patogeno = patogenoService.recuperarPatogeno(patogenoMalDeDragon.id!!)
+        patogenoService.agregarEspecie(patogenoMalDeDragon.id!!, "EspecieImperius", ubicacionPrivateDrive.id!!)
+        val patogenoRecuperado = patogenoService.recuperarPatogeno(patogenoMalDeDragon.id!!)
 
         Assertions.assertEquals(patogenoRecuperado.cantidadDeEspecies, patogeno.cantidadDeEspecies + 1)
     }
@@ -75,116 +88,107 @@ class PatogenoServiceTest {
     @Test
     fun seIntentaAgregarUnaEspecieEnUnaUbicacionInexistente(){
 
-        val ex = assertThrows<NoExisteElid> { patogenoService.agregarEspecie(1,"EspecieViolenta", 400)  }
+        val ex = assertThrows<NoExisteElid> { patogenoService.agregarEspecie(patogenoMalDeDragon.id!!,"EspecieImperius", 400)  }
 
         Assertions.assertEquals("La ubicacion no existe en la base de datos", ex.message)
     }
 
     @Test
     fun alCrearUnaEspecieSeLeAsignaUnId() {
-        val patogeno = Patogeno("testEspecie")
-        val patogenoCreado = patogenoService.crearPatogeno(patogeno)
-        val ubicacionCreada = ubicacionService.crearUbicacion("ubicacionTestEspecie")
-        vectorServiceImpl.crearVector(TipoDeVector.Persona, ubicacionCreada.id!!)
-        val especieCreada = patogenoService.agregarEspecie(patogenoCreado.id!!, "cualquierNombre", ubicacionCreada.id!!)
+        val especieCreada = patogenoService.agregarEspecie(patogenoMalDeDragon.id!!, "EspecieImperius", ubicacionPrivateDrive.id!!)
 
         Assertions.assertNotNull(especieCreada.id!!)
     }
 
     @Test
     fun noSePuedeCrearUnaEspecieConNombreVacio() {
-        val patogeno = Patogeno("testEspecie")
-        val patogenoCreado = patogenoService.crearPatogeno(patogeno)
-        val ubicacionCreada = ubicacionService.crearUbicacion("ubicacionTestEspecie")
-        vectorServiceImpl.crearVector(TipoDeVector.Persona, ubicacionCreada.id!!)
-
         Assertions.assertThrows(NoPuedeEstarVacioOContenerCaracteresEspeciales::class.java) {
-            patogenoService.agregarEspecie(patogenoCreado.id!!, "", ubicacionCreada.id!!)
+            patogenoService.agregarEspecie(patogenoMalDeDragon.id!!, "", ubicacionPrivateDrive.id!!)
         }
     }
 
     @Test
     fun noSePuedeCrearUnaEspecieConNombreConCaracteresEspeciales() {
-        val patogeno = Patogeno("testEspecie")
-        val patogenoCreado = patogenoService.crearPatogeno(patogeno)
-        val ubicacionCreada = ubicacionService.crearUbicacion("ubicacionTestEspecie")
-        vectorServiceImpl.crearVector(TipoDeVector.Persona, ubicacionCreada.id!!)
 
         Assertions.assertThrows(NoPuedeEstarVacioOContenerCaracteresEspeciales::class.java) {
-            patogenoService.agregarEspecie(patogenoCreado.id!!, "", ubicacionCreada.id!!)
+            patogenoService.agregarEspecie(patogenoMalDeDragon.id!!, "especie#1", ubicacionPrivateDrive.id!!)
         }
         Assertions.assertThrows(NoPuedeEstarVacioOContenerCaracteresEspeciales::class.java) {
-            patogenoService.agregarEspecie(patogenoCreado.id!!, "especie#1", ubicacionCreada.id!!)
+            patogenoService.agregarEspecie(patogenoMalDeDragon.id!!, "especie--1", ubicacionPrivateDrive.id!!)
         }
         Assertions.assertThrows(NoPuedeEstarVacioOContenerCaracteresEspeciales::class.java) {
-            patogenoService.agregarEspecie(patogenoCreado.id!!, "especie--1", ubicacionCreada.id!!)
-        }
-        Assertions.assertThrows(NoPuedeEstarVacioOContenerCaracteresEspeciales::class.java) {
-            patogenoService.agregarEspecie(patogenoCreado.id!!, "especie@1", ubicacionCreada.id!!)
+            patogenoService.agregarEspecie(patogenoMalDeDragon.id!!, "especie@1", ubicacionPrivateDrive.id!!)
         }
     }
 
     @Test
     fun noSePuedeCrearUnaEspecieConUnPatogenoConIdInvalido() {
-        val patogeno = Patogeno("testEspecie")
-        val patogenoCreado = patogenoService.crearPatogeno(patogeno)
-
         dataService.eliminarTodo()
 
-        val ubicacionCreada = ubicacionService.crearUbicacion("ubicacionTestEspecie")
-        vectorServiceImpl.crearVector(TipoDeVector.Persona, ubicacionCreada.id!!)
+        val ubicacionHogsmeade = ubicacionService.crearUbicacion("ubicacionHogsmeade")
+        vectorServiceImpl.crearVector(TipoDeVector.Persona, ubicacionHogsmeade.id!!)
 
         Assertions.assertThrows(NoExisteElid::class.java) {
-            patogenoService.agregarEspecie(patogenoCreado.id!!, "unNombreEspecie", ubicacionCreada.id!!)
+            patogenoService.agregarEspecie(patogenoMalDeDragon.id!!, "unNombreEspecie", ubicacionHogsmeade.id!!)
         }
     }
 
     @Test
     fun noSePuedeCrearUnaEspecieEnUnaUbicacionSinVectores() {
-        val patogeno = Patogeno("testEspecie")
-        val patogenoCreado = patogenoService.crearPatogeno(patogeno)
-        val ubicacionCreada = ubicacionService.crearUbicacion("ubicacionTestEspecie")
-
         Assertions.assertThrows(NingunVectorAInfectarEnLaUbicacionDada::class.java) {
-            patogenoService.agregarEspecie(patogenoCreado.id!!, "unNombreEspecie", ubicacionCreada.id!!)
+            patogenoService.agregarEspecie(patogenoMalDeDragon.id!!, "EspecieImperius", ubicacionLaMadriguera.id!!)
         }
     }
 
     @Test
     fun esPandemiaAfirmativo(){
-        val unVector = vectorServiceImpl.recuperarVector(6)
-        val otroVector = vectorServiceImpl.recuperarVector(7)
-        val otroVectormas = vectorServiceImpl.recuperarVector(5)
-        val otroVectorcitoMas = vectorServiceImpl.recuperarVector(4)
-        val unaEspecie = especieServiceImpl.recuperarEspecie(1)
+        dataService.eliminarTodo()
+        patogenoMalDeDragon = patogenoService.crearPatogeno(Patogeno("MalDeDragon"))
+        ubicacionPrivateDrive = ubicacionService.crearUbicacion("PrivateDrive")
+        vectorHarryPotter = vectorServiceImpl.crearVector(TipoDeVector.Persona, ubicacionPrivateDrive.id!!)
+        ubicacionLaMadriguera = ubicacionService.crearUbicacion("ubicacionLaMadriguera")
+        vectorRonWeasley = vectorServiceImpl.crearVector(TipoDeVector.Persona, ubicacionLaMadriguera.id!!)
+        val especieImperius = patogenoService.agregarEspecie(patogenoMalDeDragon.id!!, "EspecieImperius", ubicacionPrivateDrive.id!!)
+        val u = ubicacionService.recuperarTodos()
+        vectorRonWeasley.infectarCon(especieImperius)
+        vectorHarryPotter.infectarCon(especieImperius)
+        //val unVector = vectorServiceImpl.recuperarVector(6)
+        //val otroVector = vectorServiceImpl.recuperarVector(7)
+        //val otroVectormas = vectorServiceImpl.recuperarVector(5)
+        //val otroVectorcitoMas = vectorServiceImpl.recuperarVector(4)
+        //val unaEspecie = especieServiceImpl.recuperarEspecie(1)
 
-        vectorServiceImpl.infectar(otroVector, unaEspecie)
-        vectorServiceImpl.infectar(unVector, unaEspecie)
-        vectorServiceImpl.infectar(otroVectormas, unaEspecie)
-        vectorServiceImpl.infectar(otroVectorcitoMas, unaEspecie)
+        //vectorServiceImpl.infectar(otroVector, unaEspecie)
+        //vectorServiceImpl.infectar(unVector, unaEspecie)
+        //vectorServiceImpl.infectar(otroVectormas, unaEspecie)
+        //vectorServiceImpl.infectar(otroVectorcitoMas, unaEspecie)
 
-        Assertions.assertTrue(patogenoService.esPandemia((unaEspecie.id!!)))
+        //Assertions.assertTrue(patogenoService.esPandemia((unaEspecie.id!!)))
+        //Assertions.assertTrue(patogenoService.esPandemia((especieImperius.id!!)))
 
     }
 
     @Test
     fun esPandemiaNegativo(){
-
-        Assertions.assertFalse(patogenoService.esPandemia(1))
+        val especieImperius = patogenoService.agregarEspecie(patogenoMalDeDragon.id!!, "EspecieImperius", ubicacionPrivateDrive.id!!)
+        Assertions.assertFalse(patogenoService.esPandemia(especieImperius.id!!))
 
     }
 
     @Test
     fun seRecuperanTodosLosPatogenosExistentes() {
+        dataService.eliminarTodo()
+        patogenoMalDeDragon = patogenoService.crearPatogeno(Patogeno("MalDeDragon"))
+        patogenoVirusCruciartus = patogenoService.crearPatogeno(Patogeno("VirusCruciartus"))
+        var patogenoAvadaKedavra = patogenoService.crearPatogeno(Patogeno("AvadaKedavra"))
 
         val listaDePatogenosRecuperados = patogenoService.recuperarATodosLosPatogenos()
 
-        Assertions.assertNotNull(listaDePatogenosRecuperados.find { it.id == 1.toLong() })
-        Assertions.assertNotNull(listaDePatogenosRecuperados.find { it.id == 2.toLong() })
-        Assertions.assertNotNull(listaDePatogenosRecuperados.find { it.id == 3.toLong() })
-        Assertions.assertNotNull(listaDePatogenosRecuperados.find { it.id == 4.toLong() })
+        Assertions.assertNotNull(listaDePatogenosRecuperados.find { it.id == patogenoMalDeDragon.id })
+        Assertions.assertNotNull(listaDePatogenosRecuperados.find { it.id == patogenoVirusCruciartus.id })
+        Assertions.assertNotNull(listaDePatogenosRecuperados.find { it.id == patogenoAvadaKedavra.id })
 
-        Assertions.assertEquals(listaDePatogenosRecuperados.size, 4)
+        Assertions.assertEquals(3, listaDePatogenosRecuperados.size)
 
     }
 
@@ -197,18 +201,21 @@ class PatogenoServiceTest {
 
     @Test
     fun seIntentanRecuperarLasEspeciesDeUnPatogenoSinEspecies() {
-        val listaDeEspeciesRecuperadas = patogenoService.especiesDePatogeno(4)
+
+        val listaDeEspeciesRecuperadas = patogenoService.especiesDePatogeno(patogenoMalDeDragon.id!!)
 
         Assertions.assertTrue(listaDeEspeciesRecuperadas.isEmpty())
     }
 
     @Test
     fun seRecuperanTodasLasEspeciesDeUnPatogeno() {
+        val especieImperius = patogenoService.agregarEspecie(patogenoMalDeDragon.id!!, "Imperius", ubicacionPrivateDrive.id!!)
+        val especieCruciartus = patogenoService.agregarEspecie(patogenoMalDeDragon.id!!, "Cruciartus", ubicacionPrivateDrive.id!!)
 
-        val listaDeEspeciesRecuperadas = patogenoService.especiesDePatogeno(1)
+        val listaDeEspeciesRecuperadas = patogenoService.especiesDePatogeno(patogenoMalDeDragon.id!!)
 
-        Assertions.assertNotNull(listaDeEspeciesRecuperadas.find { it.id == 1.toLong() })
-        Assertions.assertNotNull(listaDeEspeciesRecuperadas.find { it.id == 6.toLong() })
+        Assertions.assertNotNull(listaDeEspeciesRecuperadas.find { it.id == especieImperius.id })
+        Assertions.assertNotNull(listaDeEspeciesRecuperadas.find { it.id == especieCruciartus.id })
 
         Assertions.assertTrue(listaDeEspeciesRecuperadas.size == 2)
 
@@ -216,20 +223,21 @@ class PatogenoServiceTest {
 
     @Test
     fun alAgregarleUnaEspecieAUnPatogenoSeInfectaUnVectorEnLaUbicacionDada(){
-        val vectorPrevioAInfectarse = vectorServiceImpl.recuperarVector(7)
 
-        Assertions.assertFalse(vectorPrevioAInfectarse.tieneEfermedad(6))
+        Assertions.assertTrue(vectorHarryPotter.estaSano())
 
-        val especie = patogenoService.agregarEspecie(1, "virus", 8)
-        val vector = vectorServiceImpl.recuperarVector(7)
+        val especieImperius = patogenoService.agregarEspecie(patogenoMalDeDragon.id!!, "Imperius", ubicacionPrivateDrive.id!!)
 
-        Assertions.assertTrue(vector.tieneEfermedad(especie.id!!))
+        val vectorHarryPotterEnfermo = vectorServiceImpl.recuperarVector(vectorHarryPotter.id!!)
+
+        Assertions.assertTrue(vectorHarryPotterEnfermo.tieneEfermedad(especieImperius.id!!))
     }
 
     @Test
     fun alAgregarleUnaEspecieAUnPatogenoSeIntentaInfectarAUnVectorPeroNoHayNingunoEnLaUbicacionDada(){
+        val ubicacionHogwarts = ubicacionService.crearUbicacion("Hogwarts")
 
-        val ex = assertThrows<NingunVectorAInfectarEnLaUbicacionDada> { patogenoService.agregarEspecie(1, "virus", 9)  }
+        val ex = assertThrows<NingunVectorAInfectarEnLaUbicacionDada> { patogenoService.agregarEspecie(patogenoMalDeDragon.id!!, "Imperius", ubicacionHogwarts.id!!)  }
 
         Assertions.assertEquals("No hay ningún vector en la ubicación dada", ex.message)
 
