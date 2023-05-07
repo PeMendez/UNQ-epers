9a2d3bd6a9d58a98490171bdbeba21b2727f4d5d
Pablo <pablo.bragoni@gmail.com>
2023-05-07 19:51:59 -0300

    modifique la manera en que la que se infecta un vector a otro

--- src/main/java/ar/edu/unq/eperdemic/modelo/Vector.kt
+++ src/main/java/ar/edu/unq/eperdemic/modelo/Vector.kt
@@ -7,24 +7,25 @@ class Vector(var tipo: TipoDeVector,
              @ManyToOne
              var ubicacion: Ubicacion,
              @ManyToMany(fetch = FetchType.EAGER)
-             var especies : MutableSet<Especie> = mutableSetOf(),
-             @ManyToMany(fetch = FetchType.EAGER)
-             var mutaciones : MutableSet<Mutacion> = mutableSetOf()) {
+             var especies : MutableSet<Especie> = mutableSetOf())
+             //@ManyToMany(fetch = FetchType.EAGER)
+             //var mutaciones : MutableSet<Mutacion> = mutableSetOf())
+             {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
 
-    fun intentarInfectar(vectorInfectado: Vector, especie: Especie) {
-        if (esContagioExitoso(vectorInfectado,especie)) {
-            vectorInfectado.mutar(especie)
-            this.infectarCon(especie)
+    fun intentarInfectar(vectorAInfectar: Vector, especie: Especie) {
+        if (esContagioExitoso(vectorAInfectar,especie)) {
+            //vectorInfectado.mutar(especie)
+            vectorAInfectar.infectarCon(especie)
         }
     }
 
     private fun mutar(especie: Especie) : Boolean {
         if (esMutacionExitosa(especie)){
-            mutaciones.add()
+            //mutaciones.add()
         }
         return Random.decidir(100) < especie.capacidadDeBiomecanizacion()
     }
@@ -41,9 +42,9 @@ class Vector(var tipo: TipoDeVector,
         especies.add(especie)
     }
 
-    fun esContagioExitoso(vectorInfectado: Vector, especie: Especie): Boolean {
-        return ubicacion.nombre == vectorInfectado.ubicacion.nombre
-                && tipo.puedeSerInfectado(vectorInfectado.tipo)
+    fun esContagioExitoso(vectorAInfectar: Vector, especie: Especie): Boolean {
+        return ubicacion.nombre == vectorAInfectar.ubicacion.nombre
+                && vectorAInfectar.tipo.puedeSerInfectado(tipo)
                 && Random.decidir(100) < porcentajeDeContagioExitoso(especie)
     }
 
@@ -60,8 +61,11 @@ class Vector(var tipo: TipoDeVector,
         this.ubicacion = ubicacion
 
     }
-
-
+    fun intentarInfectarConEspeciesDeVector(vectorAInfectar:Vector) {
+        this.especies.forEach { e ->
+            vectorAInfectar.intentarInfectar(vectorAInfectar, e)
+        }
+    }
 }
 
 enum class TipoDeVector {
