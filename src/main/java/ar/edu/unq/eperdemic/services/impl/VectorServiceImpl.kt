d450ad9f72560d0c842e5fe0fe890633c54e8362
Ornella Mendez <hope.peque@gmail.com>
2023-05-09 18:39:08 -0300

    vectorTest pasando todo hasta el momento

--- src/main/java/ar/edu/unq/eperdemic/services/impl/VectorServiceImpl.kt
+++ src/main/java/ar/edu/unq/eperdemic/services/impl/VectorServiceImpl.kt
@@ -4,6 +4,7 @@ import ar.edu.unq.eperdemic.modelo.Especie
 import ar.edu.unq.eperdemic.modelo.TipoDeVector
 import ar.edu.unq.eperdemic.modelo.Vector
 import ar.edu.unq.eperdemic.modelo.exceptions.NoExisteElid
+import ar.edu.unq.eperdemic.persistencia.dao.EspecieDAO
 //import ar.edu.unq.eperdemic.persistencia.dao.PatogenoDAO
 import ar.edu.unq.eperdemic.persistencia.dao.UbicacionDAO
 import ar.edu.unq.eperdemic.persistencia.dao.VectorDAO
@@ -20,25 +21,31 @@ import org.springframework.transaction.annotation.Transactional
 class VectorServiceImpl(): VectorService {
 
     @Autowired
+    private lateinit var especieDAO: EspecieDAO
+
+    @Autowired
     private lateinit var vectorDAO: VectorDAO
     @Autowired
     private lateinit var ubicacionDAO: UbicacionDAO
 
     override fun contagiar(vectorInfectado: Vector, vectores: List<Vector>) {
+        vectorDAO.findByIdOrNull(vectorInfectado.id)?: throw NoExisteElid("No existe el ID del vector")
         vectores.forEach { v ->
             vectorInfectado.intentarInfectarConEspecies(v)
             vectorDAO.save(v)
         }
-        //vectorDAO.saveAll(vectores)
     }
+
     override fun infectar(vector: Vector, especie: Especie) {
+        vectorDAO.findByIdOrNull(vector.id)?: throw NoExisteElid("No existe el ID del vector")
+        especieDAO.findByIdOrNull(especie.id!!)?: throw NoExisteElid("El id de Especie ingresado no existe")
         especie.let { e ->  vector.serInfectadoCon(e)}
         vector.let { v -> vectorDAO.save(v) }
     }
 
     override fun enfermedades(vectorId: Long): List<Especie> {
-        //return vectorDAO.findEnfermedades(vectorId)
-        TODO("falla")
+        vectorDAO.findByIdOrNull(vectorId)?: throw NoExisteElid("No existe el ID del vector")
+        return vectorDAO.findEnfermedades(vectorId)
     }
 
     override fun crearVector(tipo: TipoDeVector, ubicacionId: Long): Vector {
