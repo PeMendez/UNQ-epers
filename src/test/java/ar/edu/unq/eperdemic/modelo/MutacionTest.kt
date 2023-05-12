0a09e33b3438bf1f0fa84faf13289cf5b7be5062
Franco Marengo <francomarengo.21@gmail.com>
2023-05-12 15:00:35 -0300

    Agrego dos tests pequeños en mutacion

--- src/test/java/ar/edu/unq/eperdemic/modelo/MutacionTest.kt
+++ src/test/java/ar/edu/unq/eperdemic/modelo/MutacionTest.kt
@@ -1,4 +1,23 @@
 package ar.edu.unq.eperdemic.modelo
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
 class MutacionTest {
+
+    @Test
+    fun seLeAsignaUnaEspecieAUnaMutacionCorrectamente() {
+        val mutacion = Mutacion(TipoDeMutacion.SupresionBiomecanica)
+        val especie = Especie()
+        mutacion.addEspecie(especie)
+
+        Assertions.assertEquals(especie, mutacion.especie)
+    }
+
+    @Test
+    fun unaMutacionConoceSuTipoCorrectamente() {
+        val mutacion = Mutacion(TipoDeMutacion.BioalteracionGenetica)
+
+        Assertions.assertEquals(mutacion.tipoDeMutacion, TipoDeMutacion.BioalteracionGenetica)
+    }
 }
