5eb98074bab893d56869258df21463bbe074d5af
Ornella Mendez <hope.peque@gmail.com>
2023-06-01 16:01:30 -0300

    borro constructor que está al pedo

--- src/main/java/ar/edu/unq/eperdemic/modelo/UbicacionNeo4J.kt
+++ src/main/java/ar/edu/unq/eperdemic/modelo/UbicacionNeo4J.kt
@@ -1,6 +1,5 @@
 package ar.edu.unq.eperdemic.modelo
 
-import ar.edu.unq.eperdemic.modelo.exceptions.NoPuedeEstarVacioOContenerCaracteresEspeciales
 import org.springframework.data.neo4j.core.schema.Id
 import org.springframework.data.neo4j.core.schema.Node
 import org.springframework.data.neo4j.core.schema.Relationship
@@ -11,15 +10,6 @@ class UbicacionNeo4J() {
     var id : Long? = null
     lateinit var nombre: String
     var idRelacional: Long? = null
-
-    constructor(newName: String, idRelacional: Long) : this() {
-        if (Check.validar(newName)) {
-            this.nombre = newName
-            this.idRelacional = idRelacional
-        } else {
-            throw NoPuedeEstarVacioOContenerCaracteresEspeciales("El nombre no puede ser vacío o contener caracteres especiales")
-        }
-    }
 
     //comento porque no anduvo////////////////////////////////////////////////////
     //@Relationship(type = "CAMINO", direction = Relationship.Direction.OUTGOING)
